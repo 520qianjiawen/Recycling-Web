29eafa902219fd8000f29e373d9062db9b60e902
caovn <520qianjiawen@sina.com>
2024-04-30 16:13:31 +0800

    1

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -3,7 +3,7 @@ const navBarLinks = [
   { name: "Home", url: "/" },
   { name: "Products", url: "/products" },
   { name: "Services", url: "/services" },
-  { name: "Blog", url: "/blog" },
+  { name: "Equipment", url: "/blog" },
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
