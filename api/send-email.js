83bab54b1c97bd83da485f8b1cb32cbe891f9eac
caovn <520qianjiawen@sina.com>
2024-08-16 16:13:00 +0800

    recycling

--- api/send-email.js
+++ api/send-email.js
@@ -5,17 +5,17 @@ module.exports = async (req, res) => {
     // 创建 Nodemailer 传输器配置，用于连接 Hostinger 的 SMTP 服务器
     let transporter = nodemailer.createTransport({
       host: 'smtp.hostinger.com', // Hostinger SMTP 服务器地址
-      port: 461, // SMTP 端口，通常为 587 或 465
-      secure: true, // 如果端口为 465，设为 true；否则为 false
+      port: 465, // 使用 465 端口进行 SSL 连接
+      secure: true, // 如果端口为 465，设为 true
       auth: {
-        user: process.env.HOSTINGER_EMAIL, // 使用环境变量来存储你的 Hostinger 邮箱账户
-        pass: process.env.HOSTINGER_EMAIL_PASSWORD // 使用环境变量来存储你的邮箱密码
+        user: 'cavon@recycling.top', // 直接硬编码你的 Hostinger 邮箱账户
+        pass: '912..Cavon' // 直接硬编码你的邮箱密码
       }
     });
 
     // 设置邮件内容
     const mailOptions = {
-      from: process.env.HOSTINGER_EMAIL, // 发件人地址也使用环境变量
+      from: 'cavon@recycling.top', // 发件人地址
       to: 'sales@rumtoo.com', // 收件人地址
       subject: '新的表单提交', // 邮件主题
       text: `收到来自姓名: ${req.body.firstName} 的表单提交` // 邮件正文
