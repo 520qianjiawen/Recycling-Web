28c4f9becdaa26bad0df16c8a4f94d8b7036e6d3
caovn <520qianjiawen@sina.com>
2024-08-19 10:38:37 +0800

    recycling

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,26 +8,26 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://www.recyclingmachine.xyz",
+  site: "https://screwfast.uk",
   image: {
     domains: ["images.unsplash.com"],
   },
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr"],
-    fallback: {
-      fr: "en",
-    },
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
+  // i18n: {
+  //   defaultLocale: "en",
+  //   locales: ["en", "fr"],
+  //   fallback: {
+  //     fr: "en",
+  //   },
+  //   routing: {
+  //     prefixDefaultLocale: false,
+  //   },
+  // },
   prefetch: true,
   integrations: [
     tailwind(),
     sitemap({
       i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://www.recyclingmachine.xyz/` will be treated as default locale, i.e. `en`
+        defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
         locales: {
           en: "en", // The `defaultLocale` value must present in `locales` keys
           fr: "fr",
@@ -35,8 +35,13 @@ export default defineConfig({
       },
     }),
     starlight({
-      title: "Rumtoo Machinery",
+      title: "ScrewFast Docs",
       defaultLocale: "root",
+      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
+      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
+      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
+      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
+      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
       locales: {
         root: {
           label: "English",
@@ -80,28 +85,30 @@ export default defineConfig({
         },
       ],
       social: {
-        github: "https://github.com/",
+        github: "https://github.com/mearashadowfax/ScrewFast",
       },
       disable404Route: true,
-      customCss: ["./src/styles/starlight.css"],
+      customCss: ["./src/assets/styles/starlight.css"],
       favicon: "/favicon.ico",
       components: {
         SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
         Head: "./src/components/ui/starlight/Head.astro",
+        MobileMenuFooter: "./src/components/ui/starlight/MobileMenuFooter.astro",
+        ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
       },
       head: [
         {
           tag: "meta",
           attrs: {
             property: "og:image",
-            content: "https://www.recyclingmachine.xyz" + "/social.webp",
+            content: "https://screwfast.uk" + "/social.webp",
           },
         },
         {
           tag: "meta",
           attrs: {
             property: "twitter:image",
-            content: "https://www.recyclingmachine.xyz" + "/social.webp",
+            content: "https://screwfast.uk" + "/social.webp",
           },
         },
       ],
